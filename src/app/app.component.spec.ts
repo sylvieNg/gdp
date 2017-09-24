297fc5a7e69653ec432acaf55c5cb3b8828d80b3
unknown <sylvieng81@gmail.com>
2017-09-24 19:52:32 +0800

    first version of job ads

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,9 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DebugElement, NgZone, EventEmitter } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(() => {
@@ -9,6 +12,10 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule
+      ]
     });
     TestBed.compileComponents();
   });
@@ -19,16 +26,78 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
+  it(`test unilever 3 classic 1 premium`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    const app = fixture.componentInstance;
+    app.jobAds = [{id: 'classic', name:'Classic Ad', price:'269.99'}, 
+    {id: 'standout', name:'Standout Ad', price:'322.99'}, 
+    {id: 'premium', name:'Premium Ad', price:'394.99'}];
+
+    app.jobAdsArray = [{id: 'classic', name:'Classic Ad', price:'269.99'},
+    {id: 'classic', name:'Classic Ad', price:'269.99'},
+    {id: 'classic', name:'Classic Ad', price:'269.99'},
+    {id: 'premium', name:'Premium Ad', price:'394.99'}];
+    app.form.patchValue({
+      customerName: 'unilever'
+    })
+    app.submitForm();
+    expect(app.totalPrice).toBe(934.97)
+  }));
+
+  it('test apple 3 standout 1 premium', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    app.jobAds = [{id: 'classic', name:'Classic Ad', price:'269.99'}, 
+    {id: 'standout', name:'Standout Ad', price:'322.99'}, 
+    {id: 'premium', name:'Premium Ad', price:'394.99'}];
+
+    app.jobAdsArray = [{id: 'standout', name:'Standout Ad', price:'322.99'},
+    {id: 'standout', name:'Standout Ad', price:'322.99'},
+    {id: 'standout', name:'Standout Ad', price:'322.99'},
+    {id: 'premium', name:'Premium Ad', price:'394.99'}];
+    app.form.patchValue({
+      customerName: 'Apple'
+    })
+    app.submitForm();
+    expect(app.totalPrice).toBe(1294.96)
+  }));
+
+  it('test nike 4 premium', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    app.jobAds = [{id: 'classic', name:'Classic Ad', price:'269.99'}, 
+    {id: 'standout', name:'Standout Ad', price:'322.99'}, 
+    {id: 'premium', name:'Premium Ad', price:'394.99'}];
+
+    app.jobAdsArray = [{id: 'premium', name:'Premium Ad', price:'394.99'},
+    {id: 'premium', name:'Premium Ad', price:'394.99'},
+    {id: 'premium', name:'Premium Ad', price:'394.99'},
+    {id: 'premium', name:'Premium Ad', price:'394.99'}];
+    app.form.patchValue({
+      customerName: 'nike'
+    })
+    app.submitForm();
+    expect(app.totalPrice).toBe(1519.96)
+  }));
+
+  it('test default 1 classic 1 standout 1 premium', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    app.jobAds = [{id: 'classic', name:'Classic Ad', price:'269.99'}, 
+    {id: 'standout', name:'Standout Ad', price:'322.99'}, 
+    {id: 'premium', name:'Premium Ad', price:'394.99'}];
+
+    app.jobAdsArray = [{id: 'classic', name:'Classic Ad', price:'269.99'}, 
+    {id: 'standout', name:'Standout Ad', price:'322.99'}, 
+    {id: 'premium', name:'Premium Ad', price:'394.99'}];
+    app.form.patchValue({
+      customerName: 'default'
+    })
+    app.submitForm();
+    expect(app.totalPrice).toBe(987.97)
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  // title = 'app works!';
+  jobAds = [{id: 'classic', name:'Classic Ad', price:'269.99'}, 
+  {id: 'standout', name:'Standout Ad', price:'322.99'}, 
+  {id: 'premium', name:'Premium Ad', price:'394.99'}]
+  jobAdsArray = [];
+  form: FormGroup;
+  totalPrice: number = 0;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(){
+    this.form = this.formBuilder.group({
+      customerName: new FormControl(''),
+    });
+  }
+
+  addJobAds(ads){
+    this.jobAdsArray.push(ads)
+  }
+
+  removeJobAds(selected, index){
+    for(let i = 0; i <= this.jobAdsArray.length; i++){
+      if(i === index){
+        this.jobAdsArray.splice(index, 1)
+      }
+    }
+  }
+
+  reset(){
+    this.ngOnInit();
+    this.jobAdsArray = [];
+    this.totalPrice = 0;
+  }
+
+  submitForm(){
+    let numberOfClassic = 0;
+    let numberOfStandout = 0;
+    let numberOfPremium = 0;
+    for(let i = 0; i < this.jobAdsArray.length; i++){
+      if(this.jobAdsArray[i]['id'] === 'classic'){
+        numberOfClassic ++;
+      } else if(this.jobAdsArray[i]['id'] === 'standout'){
+        numberOfStandout++;
+      } else {
+        numberOfPremium++;
+      }
+    }
+    switch(this.form.value.customerName.toLowerCase()){
+      case 'unilever':
+      this.totalPrice = (numberOfClassic%3 * 269.99) + (Math.trunc(numberOfClassic/3) * (269.99 * 2)) + 
+      (numberOfPremium * 394.99) + (numberOfStandout * 322.99);
+      break;
+      case 'apple':
+      this.totalPrice = (numberOfStandout * 299.99) + (numberOfClassic * 269.99) + (numberOfPremium * 394.99);
+      break;
+      case 'nike':
+      if(numberOfPremium >= 4){
+        this.totalPrice = (numberOfPremium * 379.99) + (numberOfClassic * 269.99) + (numberOfStandout * 322.99);
+      } else {
+        this.totalPrice = (numberOfPremium * 394.99) + (numberOfClassic * 269.99) + (numberOfStandout * 322.99);        
+      }
+      break;
+      case 'ford':
+      this.totalPrice = (numberOfClassic%5 * 269.99) + (Math.trunc(numberOfClassic/5) * (309.99 * 4)) + (numberOfStandout * 309.99);
+      if(numberOfPremium >= 3){
+        this.totalPrice += numberOfPremium * 389.99;
+      } else {
+        this.totalPrice += numberOfPremium * 394.99;
+      }
+      break;
+      default:
+      this.totalPrice = numberOfClassic * 269.99 + numberOfPremium * 394.99 + numberOfStandout * 322.99;
+      break;
+    }
+  }
 }
